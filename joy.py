51f81efdeeac710124d6b777ccd3dcc27499487a
Kenneth Centurion <kcenturion@yahoo.com>
2019-01-01 21:03:15 -0600

    hi

--- joy.py
+++ joy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 
 import sys
-import time
 import threading
 import getopt
 import serial
@@ -10,50 +9,55 @@ import urllib2
 import requests
 from sys import stdin
 
-device = '' # /dev/ttyACM0
-baud   = '' #9600
+device = ''
+baud = ''
 remoteHost = 'http://192.168.1.237:7482'
 
-#serialIn thread
+
+# serialIn thread - Thread that handles data coming from the Arduino.
 class serialIn (threading.Thread):
     def __init__(self, threadID, name,device,baud):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name     = name
         self.arduino  = connect(self.name,device,baud)
+        postConnect()
+
     def run(self):
         if not self.arduino:
             return 1
 
-        if(receiveData(self.arduino)):
+        if receiveData(self.arduino):
             return 1
 
-        arduino.close()
+        self.arduino.close()
 
-#serialOut thread
+
+# serialOut thread - Thread that handles messages going to the Arduino.
 class serialOut (threading.Thread):
-    def __init__(self, threadID, name,device,baud):
+    def __init__(self, threadID, name, device, baud):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.name     = name
-        self.arduino  = connect(self.name,device,baud)
+        self.name = name
+        self.arduino = connect(self.name, device, baud)
+
     def run(self):
         if not self.arduino:
             return 1
 
-        if(sendData(self.arduino)):
+        if sendData(self.arduino):
             return 1
 
-        arduino.close()
+        self.arduino.close()
 
-#Connect
+
+# Connect
 def connect(threadName,device,baud):
     try:
-        arduino = serial.Serial(device,baud,timeout=0)
+        arduino = serial.Serial(device, baud, timeout=0)
         if arduino:
             sys.stdout.write("{SERIALPY:CONNECTED-"+threadName+"}")
             sys.stdout.flush()
-            postConnect()
             return arduino
         else:
             sys.stderr.write(threadName+"Error with call to serial.Serial during connection.")
@@ -64,7 +68,8 @@ def connect(threadName,device,baud):
 
     return None
 
-#TX - received data from stdin to send down to the arduino.
+
+# TX - received data from stdin to send down to the arduino.
 def sendData(arduino):
     while 1:
         try:
@@ -77,49 +82,45 @@ def sendData(arduino):
             sys.stderr.write("Serial timeout exception while writing.")
             return 1
 
-#RX - data received from the arduino.
-#only post data every 800ms at most so http won't be flooded.
+
+# RX - data received from the arduino.
+# only post data every 800ms at most so http won't be flooded.
 def receiveData(arduino):
-    msIntervalThreshold = 800
-    last = datetime.datetime.now()
-    while(arduino.in_waiting >0):
-        now = datetime.datetime.now()
-        diff = now - last
-        ms = diff.total_seconds() * 1000
-        if(ms > msIntervalThreshold):
-          try:
-            input = arduino.readline()
-            if(input is not None):
-                postData(input)
-                sys.stdout.write(input)
-                sys.stdout.flush()
-                last = datetime.datetime.now()
-          except serial.SerialException:
-            sys.stderr.write("Serial exception while reading. Port probably closed.")
-            return 1
+    while 1:
+      try:
+        input = arduino.readline()
+        if input is not None:
+            postData(input)
+            sys.stdout.write(input)
+            sys.stdout.flush()
+      except serial.SerialException:
+        sys.stderr.write("Serial exception while reading. Port probably closed.")
+        return 1
 
 
-#POST to remote server
-#Convert 1:3400 -> servo:1,pos:3400
+# POST to remote server
+# Convert 1:3400 -> servo:1,pos:3400
 def postData(cmd):
-    if(cmd is not None):
-      cmd = cmd.replace('\r\n','')
+    if cmd is not None:
+      cmd = cmd.replace('\r\n', '')
       splitStr = cmd.split(':')
-      if(splitStr is not None and len(splitStr) > 1):
-        if(splitStr[0].isdigit() and splitStr[1].isdigit()):
+      if splitStr is not None and len(splitStr) > 1:
+        if splitStr[0].isdigit() and splitStr[1].isdigit():
           data = {'servo':splitStr[0],'pos':splitStr[1]}
           req = urllib2.Request(remoteHost+ '/move')
           req.add_header('Content-Type', 'application/json')
-          response = urllib2.urlopen(req, json.dumps(data))
+          urllib2.urlopen(req, json.dumps(data))
 
 
 def postConnect():
-    r = requests.get(url=remoteHost+ '/connect')
+    requests.get(url=remoteHost+ '/connect')
+
 
 def postDisconnect():
-    r = requests.get(url=remoteHost+ '/disconnect')
+    requests.get(url=remoteHost+ '/disconnect')
 
-#start
+
+# Start
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'd:b', ['device=','baud='])
 except getopt.GetoptError:
@@ -136,7 +137,7 @@ if not baud or not device:
     sys.stderr.write("Device and Baud rate are required.\n")
     sys.exit(2)
 
-#start thread management
+# Start thread management
 serialInThread = serialIn(1, "SerialIn", device,baud)
 if serialInThread:
     serialInThread.start()
